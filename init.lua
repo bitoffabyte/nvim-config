8868a64fb41278f35d7a90e98a330283a8e7aee2
R Narayan <nr.rnarayan@gmail.com.com>
2023-01-02 18:52:14 +0530

    feat: Initial Commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("bitoffabyte.plugins-setup")
+require("bitoffabyte.core.options")
+require("bitoffabyte.core.keymaps")
+require("bitoffabyte.core.colorscheme")
+
---
+++ lua/bitoffabyte/core/colorscheme.lua
@@ -0,0 +1,8 @@
+vim.cmd("colorscheme nightfly")
+
+local status, _ = pcall(vim.cmd,"colorscheme nightfly")
+if not status then 
+    print("Color scheme not found")
+    return
+end
+
